f34fc12f547e62652b3ef2e33764245d2f5b79b9
kodur <chaitanyaradon89@gmail.com>
2019-05-05 19:47:15 -0600

    Added Logger Support

--- WatchDogs_MongoWrapper/mongo_wrapper.py
+++ WatchDogs_MongoWrapper/mongo_wrapper.py
@@ -84,30 +84,23 @@ class MongoWrapper:
         :param tweets: an array of tweepy objects
         :return: None
         """
-        try:
-            dt_object = datetime.strptime(tweet['DateTimeObject'], '%Y-%m-%d %H:%M:%S')
-            date_of_tweet = str(dt_object.date())
-            time_of_tweet = str(dt_object.time())
-            self.tweets_client.insert_one({
-                "tweet_id" : tweet['id'],
-                "DateTimeObject": dt_object,
-                "Date": date_of_tweet,
-                "Time": time_of_tweet,
-                "Geo": tweet['Geo'],
-                "Coordinates": tweet['Coordinates'],
-                # "Place": tweet.place.bounding_box.coordinates,
-                "Search_Text": search_string,
-                "Text": tweet['Text'],
-                "Sentiment_Value": float(tweet['Sentiment_Value']),
-                "Sentiment_Polarity": int(tweet['Sentiment_Polarity'])
-            })
-        except Exception as e:
-            if e.__class__.__name__ == 'DuplicateKeyError':
-                # print('Tried to insert duplicates')
-                pass
-            else:
-                mongo_logger = self.get_logger('Mongo Logger')
-                mongo_logger.error(str(e))
+        dt_object = datetime.strptime(tweet['DateTimeObject'], '%Y-%m-%d %H:%M:%S')
+        date_of_tweet = str(dt_object.date())
+        time_of_tweet = str(dt_object.time())
+        self.tweets_client.insert_one({
+            "tweet_id" : tweet['id'],
+            "DateTimeObject": dt_object,
+            "Date": date_of_tweet,
+            "Time": time_of_tweet,
+            "Geo": tweet['Geo'],
+            "Coordinates": tweet['Coordinates'],
+            # "Place": tweet.place.bounding_box.coordinates,
+            "Search_Text": search_string,
+            "Text": tweet['Text'],
+            "Sentiment_Value": float(tweet['Sentiment_Value']),
+            "Sentiment_Polarity": int(tweet['Sentiment_Polarity'])
+        })
+
 
     '''Insert documents into Tweets'''
     def insert_tweet_into_db(self, tweets, search_string)-> None:
